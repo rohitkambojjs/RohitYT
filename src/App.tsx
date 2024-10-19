d096cbce669c02f33ce0159187eb70debbf437b7
Rohtigit config --global user.name Rohit <techliegelord@gmail.com>
2024-10-19 15:43:51 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { Button } from './components/ui/button'
+// import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Button>Hello World</Button>
+    </>
+  )
+}
+
+export default App
